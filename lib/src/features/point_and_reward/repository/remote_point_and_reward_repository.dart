af4111de074136fc4ae9f4b7d98353897e971845
nhat.hoang@1bytesoftware.com <nhat.hoang@1bytesoftware.com>
2024-09-25 03:20:19 +0700

    refactor: wallet

--- lib/src/features/point_and_reward/repository/remote_point_and_reward_repository.dart
+++ lib/src/features/point_and_reward/repository/remote_point_and_reward_repository.dart
@@ -3,6 +3,7 @@ import 'package:jfw_flutter/src/features/point_and_reward/model/point.dart';
 import 'package:jfw_flutter/src/features/point_and_reward/model/reward.dart';
 import 'package:jfw_flutter/src/features/point_and_reward/remote/remote_point_and_reward.dart';
 import 'package:jfw_flutter/src/features/point_and_reward/repository/point_and_reward_repository.dart';
+import 'package:jfw_flutter/src/utils/type_definition.dart';
 import 'package:utils_mobile/src/func/function.dart';
 import 'package:utils_mobile/src/model/fresult.dart';
 import 'package:utils_mobile/src/model/parsed_page_model.dart';
@@ -14,7 +15,7 @@ class RemotePointAndRewardRepositoryImpl extends PointAndRewardRepository {
   final RemotePointAndReward ref;
 
   @override
-  Future<FResult<int>> getPointOfUser({required int userId}) {
+  Future<FResult<int>> getPointOfUser({required UserId userId}) {
     return ref
         .getPointOfUser(userId: userId)
         .then((value) => FResult.success(value.data as int))
@@ -33,7 +34,7 @@ class RemotePointAndRewardRepositoryImpl extends PointAndRewardRepository {
 
   @override
   Future<FResult<ParsedPageModel<FPoint>>> getPointHistories(
-      {required int userId,
+      {required UserId userId,
       required int pageNumber,
       required int pageSize}) async {
     return ref
@@ -48,7 +49,7 @@ class RemotePointAndRewardRepositoryImpl extends PointAndRewardRepository {
 
   @override
   Future<FResult<String>> redeemReward(
-      {required int pointEventId, required int userId}) async {
+      {required int pointEventId, required UserId userId}) async {
     try {
       await ref.applyRewardToUser(pointEventId: pointEventId, userId: userId);
       return FResult.success('Redeem reward successfully');
